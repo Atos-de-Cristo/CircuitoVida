b80bdd88093df2f520e32187376978f101583c46
Wesley Teixeira <contato@wesleyteixeira.com.br>
2023-05-01 01:22:02 -0300

    feat: add comp demo

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('demo')->name('demo.')->group(function () {
+        Route::view('/form', 'pages.demo.form')->name('form');
+        Route::view('/tables', 'pages.demo.tables')->name('tables');
+        Route::view('/calendar', 'pages.demo.calendar')->name('calendar');
+        Route::view('/buttons', 'pages.demo.buttons')->name('buttons');
+        Route::view('/cards', 'pages.demo.cards')->name('cards');
+    });
 });
 
 require __DIR__.'/auth.php';
