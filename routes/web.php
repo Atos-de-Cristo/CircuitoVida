9dc4440b3e4d58e06f34fbbec0c941c6ba6f8b0c
Wesley Teixeira <contato@wesleyteixeira.com.br>
2023-05-13 10:35:02 -0300

    feat: crud inicial event

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Event;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::middleware([ 'auth:sanctum', config('jetstream.auth_session'), 'verified' ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+
+    Route::get('/event', Event::class)->name('event');
 });
