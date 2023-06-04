1ceacc91131b267434a1576aaeaf31d96af914b8
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-06-04 17:24:48 -0300

    feat: iniciando novo fluxo atv e ajustando clicks

---
+++ database/migrations/2023_06_03_123214_create_questions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Activity;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Activity::class)->nullable();
+            $table->enum('type', ['aberta', 'multi']);
+            $table->string('title');
+            $table->json('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
---
+++ resources/views/livewire/event/activity/question.blade.php
@@ -0,0 +1,3 @@
+<div>
+    *** EM DESENVOLVIMENTO ***
+</div>
