9b18c5186f4129ed548f41b54c5b5517d9c66a0f
berguisonpaiva <pberguison@gmail.com>
2023-06-14 16:40:57 -0300

    card e correção

--- resources/views/livewire/event/inscription-user.blade.php
+++ resources/views/livewire/event/inscription-user.blade.php
@@ -1,71 +1,47 @@
-<div class="fixed z-40 inset-0 overflow-y-auto ease-out duration-400">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-        <div class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
 
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
 
-        <div class="
-            inline-block
-            align-bottom
-            bg-white
-            rounded-lg
-            text-left
-            overflow-hidden
-            shadow-xl
-            transform
-            transition-all
-            sm:my-8
-            sm:align-middle
-            sm:max-w-2xl
-            sm:w-full
-        "
-            role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-            <table class="table-fixed w-full">
-                <thead>
-                    <tr class="bg-gray-100">
-                        <th class="text-left px-2 w-40">#</th>
-                        <th class="text-left px-2">Nome</th>
-                        <th class="text-left px-2">Email</th>
-                        <td class="text-left px-2">Status</td>
-                        <td></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($userInscription as $item)
-                    <tr>
-                        <td class="border px-2 py-2">{{ $item->user->id }}</td>
-                        <td class="border px-2 py-2">{{ $item->user->name }}</td>
-                        <td class="border px-2 py-2">{{ $item->user->email }}</td>
-                        <td class="border px-2 py-2">{{ getStatusInscription($item->status) }}</td>
-                        <td class="border px-2 py-2">
-                            @if ($item->status == 'P')
-                                <button wire:click.prevent="approveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Aprovar</button>
-                            @endif
-                            @if ($item->status == 'L')
-                                <button wire:click.prevent="disapproveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cancelar</button>
-                            @endif
-                            @if ($item->status == 'C')
-                                <button wire:click.prevent="approveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Aprovar</button>
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
 
-            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-                    <button wire:click="closeModal()" type="button"
-                        class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                        Fechar
-                    </button>
+<x-dialog-modal id="myModal" maxWidth="2xl" closeModal="closeModal()">
+    <x-slot name="title">
+     Inscritos
+    </x-slot>
+
+    <x-slot name="content">
+        <ul class="divide-y max-h-96 divide-gray-200">
+            @foreach($userInscription as $item)
+            <li class="flex items-center justify-between border-b px-2 py-2">
+                <img class="w-8 h-8 bg-black rounded-full mr-2"
+                        src="{{ asset($item->user->profile_photo_url) }}" width="32" height="32"
+                        alt="{{ $item->user->name }}" />
+                <span class="w-full md:w-1/3 px-2 mb-2 md:mb-0">{{ $item->user->name }}</span>
+                <span class="w-full md:w-1/3 px-2 mb-2 md:mb-0">{{ $item->user->email }}</span>
+                <span class="w-full md:w-1/3 px-2 mb-2 md:mb-0">{{ getStatusInscription($item->status) }}</span>
+                <span class="w-full md:w-1/3 px-2">
+                    @if ($item->status == 'P')
+                        <button wire:click.prevent="approveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Aprovar</button>
+                    @endif
+                    @if ($item->status == 'L')
+                        <button wire:click.prevent="disapproveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cancelar</button>
+                    @endif
+                    @if ($item->status == 'C')
+                        <button wire:click.prevent="approveInscription({{ $item->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Aprovar</button>
+                    @endif
                 </span>
-            </div>
+            </li>
+            @endforeach
+        </ul>
+    </x-slot>
 
-        </div>
-    </div>
-</div>
+
+    <x-slot name="footer">
+
+        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+
+            <button wire:click.prevent="closeModal()" type="button"
+                class="btn-default">
+                Cancel
+            </button>
+        </span>
+    </x-slot>
+</x-dialog-modal>
