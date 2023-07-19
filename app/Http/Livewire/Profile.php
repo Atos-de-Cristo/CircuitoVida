9474008bf4b2e2ad531ba8bc492d9d643138e85b
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-07-19 14:16:59 -0300

    feat: add validation base form perfil

--- app/Http/Livewire/Profile.php
+++ app/Http/Livewire/Profile.php
@@ -10,6 +10,33 @@ class Profile extends Component
 {
     public $form;
 
+    protected $rules = [
+        'form.cpf'=> 'required',
+        'form.rg'=> 'required',
+        'form.sex'=> 'required',
+        'form.birth'=> 'required',
+        'form.marital_status'=> 'required',
+        'form.date_wedding'=> 'required',
+        'form.marital_status'=> 'required',
+        'form.zip_code'=> 'required',
+        'form.address'=> 'required',
+        'form.number'=> 'required',
+        'form.complement'=> 'required',
+        'form.district'=> 'required',
+        'form.city'=> 'required',
+        'form.uf'=> 'required',
+        'form.cell_phone'=> 'required',
+        'form.church_relationship'=> 'required',
+        'form.entry_date'=> 'required',
+        'form.hou_meet'=> 'required',
+        'form.baptized'=> 'required',
+        'form.accepted_jesus'=> 'required',
+        'form.leader'=> 'required',
+        'form.pastor'=> 'required',
+        'form.Schooling'=> 'required',
+        'form.profession'=> 'required',
+    ];
+
     public function getProfileServiceProperty()
     {
         return new ProfileService;
@@ -47,13 +74,13 @@ class Profile extends Component
 
     public function store()
     {
+        $this->validate();
+
         $this->profileService->store($this->form);
 
         session()->flash('message', [
             'text' => 'Perfil atualizado com sucesso.' ,
             'type' => 'success',
         ]);
-
-       
     }
 }
--- resources/views/livewire/user/profile.blade.php
+++ resources/views/livewire/user/profile.blade.php
@@ -13,12 +13,12 @@
             <div class="col-span-6 sm:col-span-3">
                 <x-label for="cpf" value="{{ __('CPF') }}" />
                 <x-input id="cpf" type="text" class="mt-1 block w-full" wire:model.defer="form.cpf" />
-                <x-input-error for="cpf" class="mt-2" />
+                <x-input-error for="form.cpf" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-3">
                 <x-label for="rg" value="{{ __('RG') }}" />
                 <x-input id="rg" type="text" class="mt-1 block w-full" wire:model.defer="form.rg" />
-                <x-input-error for="rg" class="mt-2" />
+                <x-input-error for="form.rg" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="sex" value="{{ __('Sexo') }}" />
@@ -28,13 +28,13 @@
                     <option value="masculino">Masculino</option>
                     <option value="feminino">Feminino</option>
                 </select>
-                <x-input-error for="sex" class="mt-2" />
+                <x-input-error for="form.sex" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="birth" value="{{ __('Data de Nascimento') }}" />
                 <input type="date" wire:model.defer="form.birth"
                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
-                <x-input-error for="birth" class="mt-2" />
+                <x-input-error for="form.birth" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="marital_status" value="{{ __('Estado Civil') }}" />
@@ -45,13 +45,13 @@
                     <option value="{{ $opt->name }}">{{ $opt->value }}</option>
                     @endforeach
                 </select>
-                <x-input-error for="marital_status" class="mt-2" />
+                <x-input-error for="form.marital_status" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="date_wedding" value="{{ __('Data de Casamento') }}" />
                 <input type="date" wire:model.defer="form.date_wedding"
                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
-                <x-input-error for="date_wedding" class="mt-2" />
+                <x-input-error for="form.date_wedding" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="country" value="{{ __('País') }}" />
@@ -60,37 +60,37 @@
                     <option value="">Selecione</option>
                     <option value="brasil">Brasil</option>
                 </select>
-                <x-input-error for="marital_status" class="mt-2" />
+                <x-input-error for="form.marital_status" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="zip_code" value="{{ __('CEP') }}" />
                 <x-input id="zip_code" type="text" class="mt-1 block w-full" wire:model.defer="form.zip_code" />
-                <x-input-error for="zip_code" class="mt-2" />
+                <x-input-error for="form.zip_code" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="address" value="{{ __('Endereço') }}" />
                 <x-input id="address" type="text" class="mt-1 block w-full" wire:model.defer="form.address" />
-                <x-input-error for="address" class="mt-2" />
+                <x-input-error for="form.address" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="number" value="{{ __('Número') }}" />
                 <x-input id="number" type="text" class="mt-1 block w-full" wire:model.defer="form.number" />
-                <x-input-error for="number" class="mt-2" />
+                <x-input-error for="form.number" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="complement" value="{{ __('Complemento') }}" />
                 <x-input id="complement" type="text" class="mt-1 block w-full" wire:model.defer="form.complement" />
-                <x-input-error for="complement" class="mt-2" />
+                <x-input-error for="form.complement" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="district" value="{{ __('Bairro') }}" />
                 <x-input id="district" type="text" class="mt-1 block w-full" wire:model.defer="form.district" />
-                <x-input-error for="district" class="mt-2" />
+                <x-input-error for="form.district" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="city" value="{{ __('Cidade') }}" />
                 <x-input id="city" type="text" class="mt-1 block w-full" wire:model.defer="form.city" />
-                <x-input-error for="city" class="mt-2" />
+                <x-input-error for="form.city" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="uf" value="{{ __('Estado') }}" />
@@ -99,17 +99,17 @@
                     <option value="">Selecione</option>
                     <option value="ce">CE</option>
                 </select>
-                <x-input-error for="uf" class="mt-2" />
+                <x-input-error for="form.uf" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="phone" value="{{ __('Telefone') }}" />
                 <x-input id="phone" type="text" class="mt-1 block w-full" wire:model.defer="form.phone" />
-                <x-input-error for="phone" class="mt-2" />
+                <x-input-error for="form.phone" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="cell_phone" value="{{ __('Celular') }}" />
                 <x-input id="cell_phone" type="text" class="mt-1 block w-full" wire:model.defer="form.cell_phone" />
-                <x-input-error for="cell_phone" class="mt-2" />
+                <x-input-error for="form.cell_phone" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="church_relationship" value="{{ __('Relação com a igreja') }}" />
@@ -120,13 +120,13 @@
                     <option value="{{ $opt->name }}">{{ $opt->value }}</option>
                     @endforeach
                 </select>
-                <x-input-error for="church_relationship" class="mt-2" />
+                <x-input-error for="form.church_relationship" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="entry_date" value="{{ __('Data da entrada') }}" />
                 <input type="date" wire:model.defer="form.entry_date"
                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
-                <x-input-error for="entry_date" class="mt-2" />
+                <x-input-error for="form.entry_date" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="hou_meet" value="{{ __('Entrada por') }}" />
@@ -137,12 +137,12 @@
                     <option value="{{ $opt->name }}">{{ $opt->value }}</option>
                     @endforeach
                 </select>
-                <x-input-error for="hou_meet" class="mt-2" />
+                <x-input-error for="form.hou_meet" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="church_from" value="{{ __('Igreja de onde veio') }}" />
                 <x-input id="church_from" type="text" class="mt-1 block w-full" wire:model.defer="form.church_from" />
-                <x-input-error for="church_from" class="mt-2" />
+                <x-input-error for="form.church_from" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="baptized" value="{{ __('Batizado') }}" />
@@ -152,7 +152,7 @@
                     <option value="sim">Sim</option>
                     <option value="não">Não</option>
                 </select>
-                <x-input-error for="baptized" class="mt-2" />
+                <x-input-error for="form.baptized" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="accepted_jesus" value="{{ __('Aceitou Jessus') }}" />
@@ -162,13 +162,13 @@
                     <option value="sim">Sim</option>
                     <option value="não">Não</option>
                 </select>
-                <x-input-error for="accepted_jesus" class="mt-2" />
+                <x-input-error for="form.accepted_jesus" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="date_accepted_jesus" value="{{ __('Data que aceitou Jessus') }}" />
                 <input type="date" wire:model.defer="form.date_accepted_jesus"
                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" />
-                <x-input-error for="date_accepted_jesus" class="mt-2" />
+                <x-input-error for="form.date_accepted_jesus" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="leader" value="{{ __('É lider') }}" />
@@ -178,7 +178,7 @@
                     <option value="sim">Sim</option>
                     <option value="não">Não</option>
                 </select>
-                <x-input-error for="leader" class="mt-2" />
+                <x-input-error for="form.leader" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="pastor" value="{{ __('É pastor') }}" />
@@ -188,7 +188,7 @@
                     <option value="sim">Sim</option>
                     <option value="não">Não</option>
                 </select>
-                <x-input-error for="pastor" class="mt-2" />
+                <x-input-error for="form.pastor" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="Schooling" value="{{ __('Escolaridade') }}" />
@@ -199,15 +199,15 @@
                     <option value="{{ $opt->name }}">{{ $opt->value }}</option>
                     @endforeach
                 </select>
-                <x-input-error for="Schooling" class="mt-2" />
+                <x-input-error for="form.Schooling" class="mt-2" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-label for="profession" value="{{ __('Profissão') }}" />
                 <x-input id="profession" type="text" class="mt-1 block w-full" wire:model.defer="form.profession" />
-                <x-input-error for="profession" class="mt-2" />
+                <x-input-error for="form.profession" class="mt-2" />
             </div>
-        
-          
+
+
         </x-slot>
 
 
@@ -216,7 +216,7 @@
             <x-alert-message :message="session('message')['text']" :messageType="session('message')['type']" />
             @endif
 
-            <x-button>
+            <x-button wire:click.prevent="store()">
                 {{ __('Save') }}
             </x-button>
         </x-slot>
