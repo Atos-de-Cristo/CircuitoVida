adb64ef3cbdb02e3958217a42855c600e9a29a49
berguisonpaiva <pberguison@gmail.com>
2023-07-25 17:36:38 -0300

    dashboard

--- resources/views/livewire/dashboard/admin.blade.php
+++ resources/views/livewire/dashboard/admin.blade.php
@@ -1,15 +1,12 @@
-  
-
-<div class="w-full mt-5 flex flex-col sm:flex-row gap-2">
-    <div class="flex">
-        <div class="rounded-lg p-4 w-full bg-slate-700 ">
-            <x-dashboard-bar type="bar" title="Alunos por curso" :labels="$labels" :data="$data"  />
+<div class="w-full  mt-5 flex flex-col sm:flex-row gap-2">
+    <div class="flex w-full sm:w-1/2">
+        <div class="rounded-lg p-4 w-full bg-slate-50 ">
+            <x-dashboard-bar type="bar" title="Alunos por curso" :labels="$labels" :data="$data" />
         </div>
     </div>
-    <div class="flex">
+    <div class="flex w-full sm:w-1/2">
         <div class="rounded-lg p-4 w-full bg-slate-700 ">
-            <x-dashboard-bar type="line" title="Aulas por curso"  :labels="$labels" :data="$lessons"  />
+            <x-dashboard-bar type="line" title="Aulas por curso" :labels="$labels" :data="$lessons" />
         </div>
     </div>
-   
-  </div>
+</div>
