f5b3027566decc6c8cb0f99590ae1f83a20dbefe
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-08-23 23:04:52 -0300

    feat: migrate video controls

---
+++ database/migrations/2023_08_23_201904_create_video_controls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Lesson;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('video_controls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Lesson::class);
+            $table->integer('time_start');
+            $table->integer('time_end');
+            $table->integer('time_assisted');
+            $table->integer('time_video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('video_controls');
+    }
+};
