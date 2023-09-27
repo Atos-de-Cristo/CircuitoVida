e2b71570ca7773ce31ca50fd0312962649c686de
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-09-27 02:07:46 -0300

    fix: corrigindo player youtube

--- resources/views/livewire/event/classroom/video-player.blade.php
+++ resources/views/livewire/event/classroom/video-player.blade.php
@@ -1,55 +1,75 @@
 <div>
-    <div id="{{ $playerId }}"></div>
+    <iframe
+        id="{{ $playerId }}"
+        type="text/html"
+        width="100%"
+        height="600px"
+        src="http://www.youtube.com/embed/{{ $videoId }}?enablejsapi=1&origin=https://circuitovida.atosdecristo.com"
+        frameborder="0"
+    ></iframe>
 
     @push('scripts')
         <script>
             var player;
 
             function onYouTubeIframeAPIReady() {
-                player = new YT.Player('{{ $playerId }}', {
-                    height: '600px',
-                    width: '100%',
-                    videoId: '{{ $videoId }}',
-                    events: {
-                        'onReady': onPlayerReady,
-                        'onStateChange': onPlayerStateChange
-                    },
-                    playerVars: {
-                        autoplay: 0,
-                        controls: 2,
-                        rel: 0,
-                        modestbranding: 1,
-                        showinfo: 0,
-                        enablejsapi: 0,
-                        iv_load_policy: 3,
-                        disablekb: 0
-                    },
-                });
+                try {
+                    player = new YT.Player('{{ $playerId }}', {
+                        events: {
+                            'onReady': onPlayerReady,
+                            'onStateChange': onPlayerStateChange
+                        },
+                        playerVars: {
+                            autoplay: 0,
+                            controls: 2,
+                            rel: 0,
+                            modestbranding: 1,
+                            showinfo: 0,
+                            enablejsapi: 0,
+                            iv_load_policy: 3,
+                            disablekb: 0
+                        },
+                    });
+                } catch (e) {
+                    console.erro(e)
+                }
             }
 
             function onPlayerReady(event) {
-                window.livewire.emit('onPlayerReady', event.target.getDuration());
+                try {
+                    window.livewire.emit('onPlayerReady', event.target.getDuration());
+                } catch (e) {
+                    console.error(e)
+                }
             }
 
             function onPlayerStateChange(event) {
-                switch (event.data) {
-                    case YT.PlayerState.PLAYING:
-                        window.livewire.emit('playerStateChanged', '01');
-                        break;
-                    case YT.PlayerState.PAUSED:
-                        window.livewire.emit('playerStateChanged', '02');
-                        break;
-                    case YT.PlayerState.ENDED:
-                        window.livewire.emit('playerStateChanged', '03');
-                        break;
-                    default:
-                        window.livewire.emit('playerStateChanged', '04');
-                        break;
+                try {
+                    switch (event.data) {
+                        case YT.PlayerState.PLAYING:
+                            window.livewire.emit('playerStateChanged', '01');
+                            break;
+                        case YT.PlayerState.PAUSED:
+                            window.livewire.emit('playerStateChanged', '02');
+                            break;
+                        case YT.PlayerState.ENDED:
+                            window.livewire.emit('playerStateChanged', '03');
+                            break;
+                        default:
+                            window.livewire.emit('playerStateChanged', '04');
+                            break;
+                    }
+                } catch (e) {
+                    console.error(e)
                 }
             }
 
             window.addEventListener('beforeunload', function (event) {
-                window.livewire.emit('playerStateChanged', '03');
+                try {
+                    window.livewire.emit('playerStateChanged', '03');
+                } catch (e) {
+                    console.error(e)
+                }
             });
         </script>
     @endpush
