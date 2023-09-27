cdd2c963c4501e4c0d8d6ac941a0951b75dca204
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-09-27 02:10:56 -0300

    fix: corrigindo player youtube

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
