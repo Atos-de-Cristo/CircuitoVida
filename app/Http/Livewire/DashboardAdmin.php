cf34beea1cc06fc2c1fe8cef45384ae7890dc053
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2023-09-28 16:59:45 -0300

    refactor: organizando cod

--- app/Http/Livewire/DashboardAdmin.php
+++ app/Http/Livewire/DashboardAdmin.php
@@ -9,40 +9,33 @@ use Livewire\Component;
 
 class DashboardAdmin extends Component
 {
+    public array $labels =[];
+    public array $data =[];
+    public array $lessons =[];
+
     public function getEventServiceProperty()
     {
         return new EventService;
     }
+
     public function getUserServiceProperty()
     {
         return new UserService;
     }
+
     public function getLessonServiceProperty()
     {
         return new LessonService;
     }
-    public array $labels =[];
-    public array $data =[];
-    public array $lessons =[];
-   
+
     public function mount()
     {
         $events = $this->eventService->getLessonsWithCounts();
 
-        foreach ($events as $event) {
-            $eventName = $event->name;
-            $inscriptionsCount = $event->inscriptions_count;
-            $lessonsCount = $event->lessons_count;
-
-            
-        }
         $this->labels = $events->pluck('name')->toArray();
         $this->data = $events->pluck('inscriptions_count')->toArray();
         $this->data = $events->pluck('lessons_count')->toArray();
-    
-        
     }
-    
 
     public function getMonitoresProperty()
     {
@@ -62,15 +55,12 @@ class DashboardAdmin extends Component
         $students = $this->userService->getStudents();
         return $students->count();
     }
+
     public function getEventsProperty()
     {
         $events = $this->eventService->getAll();
         return $events->count();
     }
-
-
-
-    
 
     public function render()
     {
--- resources/views/livewire/dashboard/admin.blade.php
+++ resources/views/livewire/dashboard/admin.blade.php
@@ -1,54 +1,54 @@
 
- <div class="mx-auto max-w-screen-2xl">
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+<div class="mx-auto max-w-screen-2xl">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         <div class="dark:bg-slate-700 bg-white rounded-lg p-4  flex justify-between items-center">
-         <div class="opacity-20 hover:scale-110">
-            <x-icon-graduation-cap class=" h-24 w-24" />
-          </div>
-          <div class="text-center mr-10">
-            <h3 class="text-5xl font-bold">{{$this->students}}</h3>
-            <p class="text-lg font-roboto">Alunos</p>
-          </div>          
+            <div class="opacity-20 hover:scale-110">
+                <x-icon-graduation-cap class=" h-24 w-24" />
+            </div>
+            <div class="text-center mr-10">
+                <h3 class="text-5xl font-bold">{{$this->students}}</h3>
+                <p class="text-lg font-roboto">Alunos</p>
+            </div>
         </div>
         <div class="dark:bg-slate-700 rounded-lg p-4 bg-white  flex justify-between items-center">
-          <div class="opacity-20 hover:scale-110">
-            <x-icon-display class=" h-20 w-20" />
-          </div>
-          <div class="text-center mr-10">
-            <h3 class="text-5xl font-bold">{{$this->events}}</h3>
-            <p class="text-lg font-roboto">Cursos</p>
-          </div>          
+            <div class="opacity-20 hover:scale-110">
+                <x-icon-display class=" h-20 w-20" />
+            </div>
+            <div class="text-center mr-10">
+                <h3 class="text-5xl font-bold">{{$this->events}}</h3>
+                <p class="text-lg font-roboto">Cursos</p>
+            </div>
         </div>
         <div class="dark:bg-slate-700 rounded-lg p-4  bg-white  flex justify-between items-center">
-          <div class=" opacity-20 hover:scale-110">
-            <x-icon-circle-play class=" h-20 w-20" />
-          </div>
-          <div class="text-center mr-10">
-            <h3 class="text-5xl font-bold">{{$this->Lessons}}</h3>
-            <p class="text-lg font-roboto">Aulas</p>
-          </div>          
+            <div class=" opacity-20 hover:scale-110">
+                <x-icon-circle-play class=" h-20 w-20" />
+            </div>
+            <div class="text-center mr-10">
+                <h3 class="text-5xl font-bold">{{$this->Lessons}}</h3>
+                <p class="text-lg font-roboto">Aulas</p>
+            </div>
         </div>
         <div class="dark:bg-slate-700 rounded-lg p-4  bg-white  flex justify-between items-center">
-          <div class="opacity-20 hover:scale-110">
-            <x-icon-person-chalkboard class="w-20 h-20" />
-          </div>
-          <div class="text-center mr-10">
-            <h3 class="text-5xl font-bold">{{$this->monitores}}</h3>
-            <p class="text-lg font-roboto">Monitores</p>
-          </div>          
+            <div class="opacity-20 hover:scale-110">
+                <x-icon-person-chalkboard class="w-20 h-20" />
+            </div>
+            <div class="text-center mr-10">
+                <h3 class="text-5xl font-bold">{{$this->monitores}}</h3>
+                <p class="text-lg font-roboto">Monitores</p>
+            </div>
         </div>
-      </div>
+    </div>
 
-<div class="w-full  mt-5 flex flex-col sm:flex-row gap-2">
-    <div class="flex w-full sm:w-1/2">
-        <div class="rounded-lg p-4 w-full bg-slate-50 ">
-            <x-dashboard-bar type="bar" title="Alunos por curso" :labels="$labels" :data="$data" />
+    <div class="w-full  mt-5 flex flex-col sm:flex-row gap-2">
+        <div class="flex w-full sm:w-1/2">
+            <div class="rounded-lg p-4 w-full bg-slate-50 ">
+                <x-dashboard-bar type="bar" title="Alunos por curso" :labels="$labels" :data="$data" />
+            </div>
+        </div>
+        <div class="flex w-full sm:w-1/2">
+            <div class="rounded-lg p-4 w-full bg-slate-700 ">
+                <x-dashboard-bar type="line" title="Aulas por curso" :labels="$labels" :data="$lessons" />
+            </div>
         </div>
     </div>
-    <div class="flex w-full sm:w-1/2">
-        <div class="rounded-lg p-4 w-full bg-slate-700 ">
-            <x-dashboard-bar type="line" title="Aulas por curso" :labels="$labels" :data="$lessons" />
-        </div>
-    </div>
-</div>
 </div>
