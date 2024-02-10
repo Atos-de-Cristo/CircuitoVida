197d9c01b4d33127472bacf620195eeb3504f833
Wesley Teixeira <contato@wesleyteixeira.com.br>
2024-02-10 17:54:32 -0300

    feat: add msn qdo nao existir pendencias no aluno

--- resources/views/livewire/event/student/status.blade.php
+++ resources/views/livewire/event/student/status.blade.php
@@ -58,23 +58,25 @@
                         </div>
                     </div>
                     @endif
-                    @foreach ($activityStatus as $activityPendent)
-                    <div class="ml-2 mt-2 flex flex-row items-center">
-                        <span
-                            class="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full ring-4 ring-white dark:ring-red-900 dark:bg-gray-700">
-                            <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                <path
-                                    d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
-                            </svg>
-                        </span>
-                        <div class="ml-3">
-                            <h3 class="font-bold leading-tight">{{ $activityPendent['lesson'] }}</h3>
-                            <p class="text-sm font-semibold">{{ $activityPendent['activity'] }}</p>
-                            <p class="text-xs italic">{{ $activityPendent['status'] }}</p>
+                    @forelse ($activityStatus as $activityPendent)
+                        <div class="ml-2 mt-2 flex flex-row items-center">
+                            <span
+                                class="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full ring-4 ring-white dark:ring-red-900 dark:bg-gray-700">
+                                <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                    <path
+                                        d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                                </svg>
+                            </span>
+                            <div class="ml-3">
+                                <h3 class="font-bold leading-tight">{{ $activityPendent['lesson'] }}</h3>
+                                <p class="text-sm font-semibold">{{ $activityPendent['activity'] }}</p>
+                                <p class="text-xs italic">{{ $activityPendent['status'] }}</p>
+                            </div>
                         </div>
-                    </div>
-                    @endforeach
+                    @empty
+                        <span class="text-red-500">Nenhuma pendência encontrada!</span>
+                    @endforelse
                 </div>
             </div>
             @endif
