c33be37eada490f719e9e5c5103a07fc748c95c5
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2024-04-15 20:18:07 -0300

    feat: ajuste msn erro login

--- lang/pt_BR/auth.php
+++ lang/pt_BR/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
+    'failed' => 'Email ou senha incorretos, verifique os dados e tente novamente!',
     'password' => 'A senha informada está incorreta.',
     'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
 
