c69645ec246aaee0f04a99c7bba6c0676980c052
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2024-05-16 19:58:27 -0300

    fix: remover questoes

--- app/Services/QuestionService.php
+++ app/Services/QuestionService.php
@@ -125,8 +125,6 @@ class QuestionService extends BaseService
 
     public function delete(string $id): void
     {
-        //TODO: travando delete até confirmacao de acao
-        throw new Error('Serviço indisponível!');
         $repo = $this->find($id);
         $repo->delete();
     }
