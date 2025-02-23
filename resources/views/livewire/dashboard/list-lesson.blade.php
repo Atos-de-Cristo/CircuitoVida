e29b1ee477bdbefe1cfe81e343dac55fe602cbda
Wesley Teixeira - Mac <contato@wesleyteixeira.com.br>
2025-02-23 15:58:21 -0300

    fix: ajuste aulas resumo dashboard aluno

--- resources/views/livewire/dashboard/list-lesson.blade.php
+++ resources/views/livewire/dashboard/list-lesson.blade.php
@@ -12,17 +12,15 @@
      
             @foreach ($this->inscriptions as $modules)
             <div class="flex flex-wrap gap-3">
-                @php
-                    $lastLesson = end($modules['lessons']);
-                @endphp
+                @foreach (array_slice($modules['lessons'], -2) as $lesson)
                 <div
                     class="relative flex flex-col p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 group">
                     <div class="group-hover:blur-sm">
                         <div class="font-semibold text-gray-700 dark:text-gray-200">{{ $modules['event'] }}</div>
                         <div class="text-gray-500 dark:text-gray-400">{{ $modules['module'] }}</div>
-                        <div class="text-gray-500 dark:text-gray-400">{{ $lastLesson['title'] }}</div>
+                        <div class="text-gray-500 dark:text-gray-400">{{ $lesson['title'] }}</div>
                     </div>
-                    <a href="{{ route('classroom', ['id' => $lastLesson['id'], 'eventId' => $modules['event_id']]) }}"
+                    <a href="{{ route('classroom', ['id' => $lesson['id'], 'eventId' => $modules['event_id']]) }}"
                         class="mt-auto font-bold text-md text-blue-500 hover:underline">
                         <div
                             class="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-lg">
@@ -30,6 +28,7 @@
                         </div>
                     </a>
                 </div>
+                @endforeach
             </div>
             @endforeach
         
